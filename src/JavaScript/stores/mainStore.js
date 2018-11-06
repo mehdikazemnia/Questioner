367d5dcbd41dc31805d8f7186a1dd636414f0342
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-06 20:26:26 +0330

    login and leaderBoard reducers required, export completed

--- src/JavaScript/stores/mainStore.js
+++ src/JavaScript/stores/mainStore.js
@@ -1,7 +1,11 @@
 const {createStore, combineReducers} = require('redux');
- const questionReducer = require('../reducers/questionReducer');
- module.exports = createStore(combineReducers({
-    question:questionReducer
-}),
-/* preloadedState, */
-+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
+
+const questionReducer = require('../reducers/questionReducer');
+const loginReducer = require('../reducers/loginReducer');
+const leaderBoardReducer = require('../reducers/leaderBoardReducer');
+
+module.exports = createStore(combineReducers({
+    login: loginReducer,
+    question: questionReducer,
+    leaderBoard: leaderBoardReducer
+}));
