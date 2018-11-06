59020de296f007b783dd8c8e7af80e3e5a9d17e1
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-06 19:08:22 +0330

    question wrapper added

---
+++ src/JavaScript/components/QuestionsWrapper.js
@@ -0,0 +1,53 @@
+const React = require('react');
+const axios = require('axios');
+
+const Question = require('./Question');
+
+class QuestionWrapper extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            questions:[]
+        };
+        console.log(this.props)
+        this.renderQuestions();
+    }
+    
+    render(){
+        return (
+            <div className='questionWrapper'>
+                {this.state.questions}
+            </div>
+        );
+    }
+
+    getQuestions(){
+        return new Promise((resolve)=>{
+            axios.get(`/${this.props.address}`).then((result)=>{
+                resolve(result.data)
+            });
+        });
+    }
+    renderQuestions(){
+        this.getQuestions().then((data)=>{
+
+            let questions = [];
+            for(let question of data){
+                questions.push(<Question 
+                    current={1}
+                    correctAnswer={question.correctAnswer} 
+                    answers={question.answers} 
+                    number={question.number}
+                    question={question.question}
+                    max={data.length} />);
+            }
+
+            this.setState({
+                questions
+            })
+        })
+    }
+}
+
+module.exports = QuestionWrapper;
