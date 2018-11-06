fa05926e6d9a42e72ff2abb537b79d6f980714b7
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-06 20:24:31 +0330

    question reducer completed

--- src/JavaScript/reducers/questionReducer.js
+++ src/JavaScript/reducers/questionReducer.js
@@ -1,31 +1,57 @@
+const ScoreComputer = require('../helpers/ScoreComputer');
+
 module.exports = (state = {
-    current: 1,
+    current: 0,
     answers: [],
     userAnswers: [],
     score: 0,
     timeIsUp: false,
-    isPaused: false
+    isPaused: true,
+    statistics: {
+        wrong: 0,
+        notAnswered: 0,
+        correct: 0
+    }
 }, action)=>{
     console.log(action)
     switch(action.type){
         case 'ADD_USER_ANSWER':
             state.answers.push(action.answer);
             state.userAnswers.push(action.userAnswer);
+
+            state.score = ScoreComputer.compute(action.userAnswer, action.answer, state.score);
+
+            let answerStatus = ScoreComputer.checkAnswer(action.userAnswer, action.answer);
+            switch(answerStatus){
+                case -1:
+                    state.statistics.wrong++;
+                break;
+                case 1:
+                    state.statistics.correct++;
+                break;
+                case 0:
+                    state.statistics.notAnswered++;
+                break;
+            }
+
+            return Object.assign({},state);
+        break;
+        case 'NEXT_QUESTION':
             state.current++;
-            return state;
+            return Object.assign({},state,{timeIsUp: false, isPaused: false});
         break;
         case 'TIME_IS_UP':
-            return Object.assign(state, {timeIsUp: true});
-        break;
-        case 'TIMER_RESET':
-            return Object.assign(state, {timeIsUp: false, isPaused: false});
+            return Object.assign({},state, {timeIsUp: true});
         break;
         case 'PAUSE_TIMER':
             state.isPaused = true;
-            return state;
+            return Object.assign({},state);
+        break;
+        case 'START':
+            return Object.assign({}, state, {current: 1, isPaused: false});
         break;
         default: 
             return state;
         break;
     }
-} 
+}
