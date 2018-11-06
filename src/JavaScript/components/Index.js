036abdeb9ee861a3903c2a0005f948c73ebe22d3
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-06 19:08:47 +0330

    Index component created and used

---
+++ src/JavaScript/components/Index.js
@@ -0,0 +1,18 @@
+const React = require('react');
+
+const Timer = require('./Timer');
+const Login = require('./Login');
+const QuestionWrapper = require('./QuestionWrapper');
+
+class Index extends React.Component{
+    render(){
+        return (
+            <div className='cont'>
+                <Timer seconds={10} isPaused={false} />
+                <QuestionWrapper address='questions.json' />
+            </div>
+        );
+    }
+}
+
+module.exports = Index;
--- src/JavaScript/index.js
+++ src/JavaScript/index.js
@@ -1,5 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-const jsx = (<div></div>);
+const Index = require('./components/Index');
+
+const jsx = (<Index />);
 ReactDOM.render(jsx, document.querySelector("#app"));
