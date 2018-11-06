1bdd449abf6676b04f77b1c5b45f4b6dbf72aaf3
Mehdi Kazemnia <10383628+mehdikazemnia@users.noreply.github.com>
2018-11-06 20:13:48 +0330

    ScoreComputer as a class, answer check added

--- src/JavaScript/helpers/scoreComputer.js
+++ src/JavaScript/helpers/scoreComputer.js
@@ -1,8 +1,21 @@
-module.exports = (answer, correctAnswer, score)=>{
-    if(answer == correctAnswer){
-        score += 2;
-    }else if(answer != 0 && answer != correctAnswer){
-        score -= 1;
+class ScoreComputer{
+    static compute(answer, correctAnswer, score){
+        if(answer == correctAnswer){
+            score += 2;
+        }else if(answer != 0 && answer != correctAnswer){
+            score -= 1;
+        }
+        return score;
     }
-    return score;
+    static checkAnswer(answer, correctAnswer){
+        if(answer == correctAnswer){
+            return 1;
+        }else if(answer != 0 && answer != correctAnswer){
+            return -1;
+        }
+        return 0;
+    }
 }
+
+
+module.exports = ScoreComputer;
